847b25fe31bd19e22bd7d08bdd02bfb72fe8f3f9
FourInchKnife <riley.j.mclain@gmail.com>
2021-05-12 17:35:24 -0400

    v1.0.0-1.16.x

---
+++ src/main/java/io/github/FourInchKnife/trueinfinity/TrueInfinity.java
@@ -0,0 +1,10 @@
+package io.github.FourInchKnife.trueinfinity;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TrueInfinity implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Infinity now works with mending!");
+	}
+}
---
+++ src/main/java/io/github/FourInchKnife/trueinfinity/mixin/InfinityEnchantmentMixin.java
@@ -0,0 +1,19 @@
+package io.github.FourInchKnife.trueinfinity.mixin;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.InfinityEnchantment;
+import net.minecraft.entity.EquipmentSlot;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(InfinityEnchantment.class)
+public abstract class InfinityEnchantmentMixin extends Enchantment{
+	public InfinityEnchantmentMixin(Rarity weight, EquipmentSlot... slotTypes) {
+		super(weight, EnchantmentTarget.BOW, slotTypes);
+	}
+
+	@Override()
+	public boolean canAccept(Enchantment other) {
+		return super.canAccept(other);
+	}
+}
